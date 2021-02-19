9f0494e0bd9cb996605be58226ae8bd3ef41f55c
Maestro Haryor <54450100+Maestroharyor@users.noreply.github.com>
2021-02-19 15:30:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// Function Declaration
+// Function Expression
+// Accepting Parameters/Arguments
+// Returning values
+// Arrow Functions
+// Accepting Events
+// Javascript Methods
+
+
+
+
+// const tripleMe = (number) => {
+//     let answer =  number * 3;
+//     console.log(answer);
+// }
+
+// tripleMe(3);
+
+// let var1 = doubleMe(5);
+// let var2 = doubleMe(3);
+
+// if (var1 > var2){
+//     console.log(`Variable 1 is greater than variable 2 as ${var1} is greater than ${var2}`);
+// } else{
+//     console.log(`Variable 1 is lesser than variable 2 as ${var1} is lesser than ${var2}`);
+// }
+
+
+// const doubleMe = number => number * 2;
+
+// console.log(doubleMe(3));
+
+
+
+// const simpleInterest = (p, r, t) => {
+//     let mySIvalue = (p * r * t)/100;
+//     console.log(mySIvalue);
+
+// }
+
+// simpleInterest(200, 42, 2);
+
+
+// const greetings = () =>{
+//     console.log("Hello")
+// }
+// greetings()
+
+
+// Accepting Events
+// let input = document.querySelector('input');
+// let heading = document.querySelector('h2');
+
+// input.addEventListener('input', ()=>{
+//     heading.innerText = `${input.value} is working`;
+// })
+
+
+//METHODS
+
+// let myObj = {
+//     name: "Maestro",
+//     age: 50,
+//     method1: function (){console.log(`My name is ${this.name} and I am ${this.age} years old`)},
+//     greetings: function(){
+//         console.log(`Greeting to all aliens. I am ${this.name}`);
+//     }
+// }
+
+// myObj.method1();
+// myObj.greetings();
+
+
+// let myName = "MAestro";
+
+// console.log(myName.toLowerCase());
+// console.log(myName.toUpperCase());
